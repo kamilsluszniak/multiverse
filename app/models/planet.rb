dc8c52d6a64856d5b6986939c556b7b4def8aeba
kamil_sluszniak <sultanaghmed@gmail.com>
2017-03-16 23:23:03 +0100

    planet #update_resources updated

--- app/models/planet.rb
+++ app/models/planet.rb
@@ -5,14 +5,14 @@ class Planet < ApplicationRecord
 
   def update_resources
     if self.resources_updated_at
-      days = self.resources_updated_at - Time.now
+      seconds = Time.now - self.resources_updated_at
       self.resources_updated_at = Time.now
-      self.metal = self.metal_lvl**1.3 * 0.8 * days
-      self.crystal = self.crystal_lvl**1.2 * 0.59 * days
+      self.metal = self.metal + self.metal_lvl**1.3 * 0.03333 * seconds
+      self.crystal = self.crystal + self.crystal_lvl**1.2 * 0.02456 * seconds
       if self.hydrogen_lvl then
-        self.hydrogen = self.hydrogen_lvl**1.1* 0.32 * days
+        self.hydrogen = self.hydrogen + self.hydrogen_lvl**1.1* 0.013333 * seconds
       end
-      self.energy = self.energy_lvl**1.15 * 40
+      self.energy = self.solar_lvl**1.15 * 40
       self.save!
     end
   end
