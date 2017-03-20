647bf22eda16b93fbab60a101a61b0bf8bb165ed
kamil_sluszniak <sultanaghmed@gmail.com>
2017-03-20 22:04:45 +0100

    planets resources migrated to float excluding energy

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170315190933) do
+ActiveRecord::Schema.define(version: 20170320201957) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,9 +21,9 @@ ActiveRecord::Schema.define(version: 20170315190933) do
     t.datetime "created_at",                       null: false
     t.datetime "updated_at",                       null: false
     t.integer  "system_id"
-    t.bigint   "metal"
-    t.bigint   "crystal"
-    t.bigint   "hydrogen"
+    t.float    "metal"
+    t.float    "crystal"
+    t.float    "hydrogen"
     t.bigint   "energy"
     t.datetime "resources_updated_at"
     t.integer  "metal_lvl",            default: 1
